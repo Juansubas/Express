78bd3de1bf4e0bc21eae21f09fa09b60e0a15eab
juansubas <juansubas@gmail.com>
2024-08-30 11:54:04 -0500

    API user basic with TypeScript Donegit add .

--- 02-express-simple-api-ts/src/index.ts
+++ 02-express-simple-api-ts/src/index.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response} from 'express'
-import { promises as fs, read} from "fs";
+import { promises as fs, read, write} from "fs";
 
 const app = express();
+
+app.use(express.json());
 
 const port : number = 3000;
 
@@ -25,18 +27,17 @@ const readData = async () : Promise<User[]> => {
     
 }
 
-const writeData = async () : Promise<void> => {
+const writeData = async (data : any) : Promise<void> => {
     try {
-        await fs.writeFile('./db/db.sjon', 'utf-8');
+        await fs.writeFile('./db/db.json', JSON.stringify(data));
     } catch (error) {
         console.error('Error Writing data', Error);
     }
 }
 
-app.get('/', async (req: Request, res: Response) => {
+app.get('/users', async (req: Request, res: Response) => {
     try {
         const data : User[] = await readData();
-        console.log(data)
         res.status(200).json(data);
     } catch (error: unknown) {
         console.error('Error :', error);
@@ -44,7 +45,70 @@ app.get('/', async (req: Request, res: Response) => {
     }
 });
 
+app.get('/users/:id', async (req: Request, res: Response) => {
+    try {
+        const data : User[] = await readData();
+        const id : number = parseInt(req.params.id);
+        const user : User | undefined = data.find((user) => {
+           return user.id === id;
+        });
+        res.status(200).json(user);
+    } catch (error : unknown) {
+        console.error('Error', error);
+        res.status(500).send('Internal Server Error');
 
+    }
+});
+
+app.post('/users', async (req: Request, res: Response) => {
+    try {
+        const data : User[] = await readData();
+        const user : Omit<User, 'id'>= req.body;
+        const newUser : User = {
+            id: data.length + 1,
+            ...user
+        }
+        data.push(newUser);
+        await writeData(data);
+        res.status(201).json(user);
+    } catch (error : unknown) {
+        console.error('Error', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.put('/users/:id', async(req: Request, res: Response) => {
+    try {
+        const data : User[] = await readData();
+        const id : number = parseInt(req.params.id);
+        const updateUser : Omit<User, 'id'> = req.body;
+        const index : number = data.findIndex( user => user.id === id);
+        //data.splice(index, 1, {id , ...updateUser});
+        data[index] = {
+            id,
+            ...updateUser
+        };
+        await writeData(data);
+        res.status(200).json({ message: 'User Updated Successfully' });
+    } catch (error : unknown) {
+        console.error('Error', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.delete('/users/:id', async(req: Request, res: Response) => {
+    try {
+        const id : number = parseInt(req.params.id);
+        const data = await readData();
+        const index : number = data.findIndex( user => user.id === id);
+        data.splice(index, 1);
+        await writeData(data);
+        res.status(200).json( {message: 'User deleted succesfully'});
+    } catch (error : unknown) {
+        console.error('Error', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 app.listen(port, () => {
     console.log('Server is running')
