e504590a9f76050fab968cb7644482af7c283f1a
juansubas <juansubas@gmail.com>
2024-08-31 19:02:48 -0500

    Corrigiendo controlers

--- 03-express-architecture-api-ts/src/index.ts
+++ 03-express-architecture-api-ts/src/index.ts
@@ -11,10 +11,10 @@ const port : number = 3000;
 
 
 app.get('/api/users', (req: Request, res: Response) => userController.getUsers(req, res));
-app.get('/api/users/:id', (req: Request, res: Response) => userController.getUsers(req, res));
-app.post('/api/users', (req: Request, res: Response) => userController.getUsers(req, res));
-app.put('/api/users/:id', (req: Request, res: Response) => userController.getUsers(req, res));
-app.delete('/api/users/:id', (req: Request, res: Response) => userController.getUsers(req, res));
+app.get('/api/users/:id', (req: Request, res: Response) => userController.getUserById(req, res));
+app.post('/api/users', (req: Request, res: Response) => userController.createUser(req, res));
+app.put('/api/users/:id', (req: Request, res: Response) => userController.actualizarUser(req, res));
+app.delete('/api/users/:id', (req: Request, res: Response) => userController.deleteUser(req, res));
 
 
 app.listen(port, () => {
